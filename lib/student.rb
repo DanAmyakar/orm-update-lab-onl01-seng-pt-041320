b327b73018a8128af11c86aad2226ae9d9cc6c57
Daniel Amyakar <dan.rockit@gmail.com>
2020-06-17 02:29:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,7 +42,7 @@ class Student
   end
   
   def self.new_from_db(row)
-    new_stud = self.new
+    new_stud = Student.new
     new_stud.id = row[0]
     new_stud.name = row[1]
     new_stud.grade = row[2]
